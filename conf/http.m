1b91bb31be39199079178f2596191723fa9b6ef9
sunahkim <pingpongvvvv@gmail.com>
2018-06-11 15:46:15 +0900

    test2

--- conf/http.m
+++ conf/http.m
@@ -2,9 +2,9 @@
 webtob1
 
 *NODE
-qpsx1   WEBTOBDIR="$WEBTOBDIR",
+qpsx1   WEBTOBDIR="$APP_ROOT/webtob",
                 SHMKEY = 54030,
-                DOCROOT="$WEBTOBDIR/docs",
+                DOCROOT="$APP_ROOT/webtob/docs",
                 PORT = "5580",
                 HTH = 1,
                 #Group = "nobody",
@@ -15,7 +15,6 @@ qpsx1   WEBTOBDIR="$WEBTOBDIR",
                 LOGGING = "log1",
                 ERRORLOG = "log2",
                 SYSLOG = "syslog"
-        #       JSVPORT = 34423
 
 *HTH_THREAD
 hth_worker
@@ -31,27 +30,23 @@ hth_worker
 htmlg           NODENAME = "qpsx1", SVRTYPE = HTML
 cgig            NODENAME = "qpsx1", SVRTYPE = CGI
 ssig            NODENAME = "qpsx1", SVRTYPE = SSI
-#jsvg           NODENAME = "qpsx1", SVRTYPE = JSV
 
 *SERVER
 html            SVGNAME = htmlg, MinProc = 2, MaxProc = 10, ASQCount = 1
 cgi             SVGNAME = cgig, MinProc = 2, MaxProc = 10, ASQCount = 1
 ssi             SVGNAME = ssig, MinProc = 2, MaxProc = 10, ASQCount = 1
-#MyGroup                SVGNAME = jsvg, MinProc = 10, MaxProc = 10
 
 *URI
-#uri1           Uri = "/",   Svrtype = HTML
-#uri2            Uri = "/examples/",  SvrType=JSV, SvrName=MyGroup
 
 *ALIAS
-alias1          URI = "/cgi-bin/", RealPath = "$WEBTOBDIR/cgi-bin/"
+alias1          URI = "/cgi-bin/", RealPath = "$APP_ROOT/webtob/cgi-bin/"
 
 *LOGGING
-syslog          Format = "SYSLOG", FileName = "$WEBTOBDIR/log/system.log_%M%%D%%Y%",
+syslog          Format = "SYSLOG", FileName = "$APP_ROOT/webtob/log/system.log_%M%%D%%Y%",
                         Option = "sync"
-log1            Format = "DEFAULT", FileName = "$WEBTOBDIR/log/access.log_%M%%D%%Y%",
+log1            Format = "DEFAULT", FileName = "$APP_ROOT/webtob/log/access.log_%M%%D%%Y%",
                         Option = "sync"
-log2            Format = "ERROR", FileName = "$WEBTOBDIR/log/error.log_%M%%D%%Y%",
+log2            Format = "ERROR", FileName = "$APP_ROOT/webtob/log/error.log_%M%%D%%Y%",
                         Option = "sync"
 
 *ERRORDOCUMENT
@@ -61,4 +56,3 @@ log2            Format = "ERROR", FileName = "$WEBTOBDIR/log/error.log_%M%%D%%Y%
 *EXT
 txt             MimeType = "text/html", SvrType = HTML
 htm             MimeType = "text/html", SvrType = HTML
-#jsp            Mimetype ="application/jsp",  Svrtype=JSV,  SvrName=MyGroup
