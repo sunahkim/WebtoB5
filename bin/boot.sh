0b4dfd7416a7453cdc479311885030578e2b4ca9
sunahkim <pingpongvvvv@gmail.com>
2018-06-14 20:15:47 -0400

    test2

--- bin/boot.sh
+++ bin/boot.sh
@@ -36,7 +36,7 @@ fi
 
 #exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
 
-(tail -f -n 0 $APP_ROOT/webtob/log/*.log* &)
-exec $APP_ROOT/webtob/bin/wsboot -V $APP_ROOT/webtob/bin/wscfl -i $APP_ROOT/webtob/config/http.m 
+#(tail -f -n 0 $APP_ROOT/webtob/log/*.log* &)
+exec $APP_ROOT/webtob/bin/wscfl -i $APP_ROOT/webtob/config/http.m $APP_ROOT/webtob/bin/wsboot -V
 echo "suna"
 # ------------------------------------------------------------------------------------------------
